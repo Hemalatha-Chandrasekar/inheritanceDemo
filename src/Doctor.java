ad5c5a9d91daf66cb542f33f9a2f80bee1edb4be
Hemalatha-Chandrasekar <hema.alliswell@gmail.com>
2024-11-28 15:34:13 -0600

    Inheritance

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
-
 public class Doctor {
+        String DoctorName;
+        String Department;
+        public void Doctor_Details() {
+            System.out.println("Doctor Details...");
+        }
 }
--- src/Surgeon.java
+++ src/Surgeon.java
@@ -1,4 +1,6 @@
-package PACKAGE_NAME;
-
-public class Surgeon {
+public class Surgeon extends Doctor {
+        void Surgeon_Details() {
+            System.out.println("Surgeon  Detail...");
+            System.out.println(Department = "Cardio");
+        }
 }
